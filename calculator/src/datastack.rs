4e789fd7cac116f042030691bbe17cdd9fc5d5b0
Johannes Zottele <official@johannes-zottele.at>
2023-05-17 22:47:57 +0200

    Add datastack unit tests

--- calculator/src/datastack.rs
+++ calculator/src/datastack.rs
@@ -83,3 +83,111 @@ impl fmt::Display for DataStack {
         Ok(())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_push_and_pop() {
+        let mut stack = DataStack::new();
+
+        stack.push(Value::Integer(10));
+        stack.push(Value::Float(20.5));
+        stack.push(Value::String("Hello".into()));
+
+        assert_eq!(stack.pop(), Some(Value::String("Hello".into())));
+        assert_eq!(stack.pop(), Some(Value::Float(20.5)));
+        assert_eq!(stack.pop(), Some(Value::Integer(10)));
+        assert_eq!(stack.pop(), None);
+    }
+
+    #[test]
+    fn test_peek() {
+        let mut stack = DataStack::new();
+
+        stack.push(Value::Integer(10));
+        assert_eq!(stack.peek(), Some(Value::Integer(10)));
+        stack.push(Value::Float(20.5));
+        assert_eq!(stack.peek(), Some(Value::Float(20.5)));
+        stack.push(Value::String("Hello".into()));
+        assert_eq!(stack.peek(), Some(Value::String("Hello".into())));
+
+        stack.pop();
+        assert_eq!(stack.peek(), Some(Value::Float(20.5)));
+    }
+
+    #[test]
+    fn test_pop_int_float_string() {
+        let mut stack = DataStack::new();
+
+        stack.push(Value::Integer(10));
+        stack.push(Value::Float(20.5));
+        stack.push(Value::String("Hello".into()));
+
+        assert_eq!(stack.pop_string(), Some("Hello".into()));
+        assert_eq!(stack.pop_string(), None);
+
+        assert_eq!(stack.pop_float(), Some(20.5));
+        assert_eq!(stack.pop_float(), None);
+
+        assert_eq!(stack.pop_int(), Some(10));
+        assert_eq!(stack.pop_int(), None);
+    }
+
+    #[test]
+    fn test_len() {
+        let mut stack = DataStack::new();
+
+        assert_eq!(stack.len(), 0);
+
+        stack.push(Value::Integer(10));
+        stack.push(Value::Float(20.5));
+        stack.push(Value::String("Hello".into()));
+
+        assert_eq!(stack.len(), 3);
+
+        stack.pop();
+        assert_eq!(stack.len(), 2);
+    }
+
+    #[test]
+    fn test_empty_pop() {
+        let mut stack = DataStack::new();
+
+        assert_eq!(stack.pop(), None);
+        assert_eq!(stack.pop_int(), None);
+        assert_eq!(stack.pop_float(), None);
+        assert_eq!(stack.pop_string(), None);
+    }
+
+    #[test]
+    fn test_type_specific_pop() {
+        let mut stack = DataStack::new();
+
+        stack.push(Value::Integer(10));
+
+        // Attempt to pop float or string from an integer value should return None
+        assert_eq!(stack.pop_float(), None);
+        assert_eq!(stack.pop_string(), None);
+
+        stack.push(Value::Float(20.5));
+
+        // Attempt to pop integer or string from a float value should return None
+        assert_eq!(stack.pop_int(), None);
+        assert_eq!(stack.pop_string(), None);
+
+        stack.push(Value::String("Hello".into()));
+
+        // Attempt to pop integer or float from a string value should return None
+        assert_eq!(stack.pop_int(), None);
+        assert_eq!(stack.pop_float(), None);
+    }
+
+    #[test]
+    fn test_peek_on_empty() {
+        let mut stack = DataStack::new();
+
+        assert_eq!(stack.peek(), None);
+    }
+}
