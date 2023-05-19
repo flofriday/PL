7b541995368db941decec59d0413ddb2c7aa2f8c
Johannes Zottele <official@johannes-zottele.at>
2023-05-19 13:50:20 +0200

    Implement decimal place construction

--- calculator/src/calculator.rs
+++ calculator/src/calculator.rs
@@ -37,43 +37,77 @@ impl<IN: Read, OUT: Write> Calculator<IN, OUT> {
     pub fn run(&mut self) {
         while let Some(cmd) = self.cmd_stream.peek() {
             match self.operation_mode {
-                ..=-2 => (),                          // decimal place construction
-                -1 => self.integer_construction(cmd), // integer construction
-                0 => (),                              // execution
-                _ => (),                              // string construction
+                ..=-2 => self.decimal_place_construction(cmd), // decimal place construction
+                -1 => self.integer_construction(cmd),          // integer construction
+                0 => (),                                       // execution
+                _ => (),                                       // string construction
             }
         }
     }
 
     pub fn integer_construction(&mut self, cmd: char) {
+        assert_eq!(self.operation_mode, -1, "Wrong operation mode");
+
         match cmd {
             '0'..='9' => {
                 let digit = cmd.to_digit(10).unwrap() as i64;
 
-                if let Some(val) = self.stack.pop_int() {
-                    let acc_int = Value::Integer((val * 10) + digit);
-                    self.stack.push(acc_int)
-                } else {
-                    // TODO: some error handling since top of stack was no integer
-                    panic!(
-                        "RUNTIME ERROR: Tried to construct integer, but top of stack isn't integer"
-                    )
-                }
-                // consume command
+                let stack_top = self.stack.pop_int().expect(
+                    "RUNTIME ERROR: Tried to construct integer, but top of stack isn't integer",
+                );
+
+                let acc_int = Value::Integer((stack_top * 10) + digit);
+                self.stack.push(acc_int);
+
+                // consume integer literal
                 self.cmd_stream.poll();
             }
             '.' => {
+                // switch to decimal place construction
                 self.operation_mode = -2;
-                // consume command
+
+                // concert top of stack to float
+                let stack_top = self.stack.pop_int().expect(
+                    "RUNTIME ERROR: Tried to construct integer, but top of stack isn't integer",
+                );
+                self.stack.push(Value::Float(stack_top as f64));
+
+                // consume `.` literal
                 self.cmd_stream.poll();
             }
-            _ => {
-                self.operation_mode = 0;
-            }
+            _ => self.operation_mode = 0, // reset operation mode without consuming
         }
     }
 
-    pub fn decimal_place_construction(&mut self, cmd: char) {}
+    pub fn decimal_place_construction(&mut self, cmd: char) {
+        assert!(self.operation_mode < -1, "Wrong operation mode");
+
+        match cmd {
+            '0'..='9' => {
+                let digit = cmd.to_digit(10).unwrap() as f64;
+
+                // construct extended floating point
+                let stack_top = self.stack.pop_float().expect(
+                    "RUNTIME ERROR: Tried to construct decimal places, but top of stack isn't float",
+                );
+                // stack_top + (digit * 10^(m + 1))
+                let new_float = stack_top + (digit * 10f64.powi(self.operation_mode as i32 + 1));
+                self.stack.push(Value::Float(new_float));
+
+                // decrease operation mode -> decease next decimal place
+                self.operation_mode -= 1;
+                // consume digit literal
+                self.cmd_stream.poll();
+            }
+            '.' => {
+                // initialize new float construction
+                self.stack.push(Value::Float(0.0));
+                self.operation_mode = -2;
+                self.cmd_stream.poll();
+            }
+            _ => self.operation_mode = 0, // just switch to execution mode
+        }
+    }
 }
 
 #[cfg(test)]
@@ -117,6 +151,11 @@ mod tests {
 
         assert_eq!(calc.cmd_stream.peek(), Some('2'), "Didn't consume command");
         assert_eq!(calc.stack.pop_int(), Some(1), "Didn't update top of stack");
+        assert_eq!(
+            calc.stack.pop(),
+            None,
+            "Did not pop stack before pushing new value"
+        );
         assert_eq!(calc.operation_mode, -1, "Did change operation mode");
 
         calc.stack.push(Value::Integer(1));
@@ -124,6 +163,11 @@ mod tests {
 
         assert_eq!(calc.cmd_stream.peek(), Some('3'), "Didn't consume command");
         assert_eq!(calc.stack.pop_int(), Some(12), "Didn't update top of stack");
+        assert_eq!(
+            calc.stack.pop(),
+            None,
+            "Did not pop stack before pushing new value"
+        );
         assert_eq!(calc.operation_mode, -1, "Did change operation mode");
 
         calc.stack.push(Value::Integer(12));
@@ -134,6 +178,11 @@ mod tests {
             calc.stack.pop_int(),
             Some(123),
             "Didn't update top of stack"
+        );
+        assert_eq!(
+            calc.stack.pop(),
+            None,
+            "Did not pop stack before pushing new value"
         );
         assert_eq!(calc.operation_mode, -1, "Did change operation mode");
     }
@@ -166,6 +215,11 @@ mod tests {
 
         assert_eq!(calc.cmd_stream.peek(), Some('a'), "Didn't consume command");
         assert_eq!(calc.stack.pop_int(), Some(1), "Didn't update top of stack");
+        assert_eq!(
+            calc.stack.pop(),
+            None,
+            "Did not pop stack before pushing new value"
+        );
         assert_eq!(calc.operation_mode, -1, "Did change operation mode");
 
         calc.stack.push(Value::Integer(1));
@@ -173,6 +227,11 @@ mod tests {
 
         assert_eq!(calc.cmd_stream.peek(), Some('a'), "Did consume command");
         assert_eq!(calc.stack.pop_int(), Some(1), "Did update stack");
+        assert_eq!(
+            calc.stack.pop(),
+            None,
+            "Did not pop stack before pushing new value"
+        );
         assert_eq!(calc.operation_mode, 0, "Didn't change operation mode");
     }
 
@@ -197,7 +256,121 @@ mod tests {
         calc.integer_construction('.');
 
         assert_eq!(calc.cmd_stream.peek(), None, "Didn't consume command");
-        assert_eq!(calc.stack.pop_int(), Some(1), "Did update stack");
+        assert_eq!(calc.stack.pop_float(), Some(1.0), "Did update stack");
+        assert_eq!(
+            calc.stack.pop(),
+            None,
+            "Did not pop stack before pushing new value"
+        );
         assert_eq!(calc.operation_mode, -2, "Didn't change operation mode");
+    }
+
+    #[test]
+    fn test_dec_place_construction() {
+        // checks if the decimal_place_construction method
+        // correctly constructs decimal places from `2`
+        // and consumes literals
+
+        let mut calc = new_calc("23", -2);
+
+        // prepare stack -> skip integer construction part
+        calc.stack.push(Value::Float(1.0));
+        calc.decimal_place_construction('2');
+
+        assert_eq!(calc.cmd_stream.peek(), Some('3'), "Didn't consume command");
+        assert_eq!(
+            calc.stack.pop_float(),
+            Some(1.2),
+            "Didn't update top of stack"
+        );
+        assert_eq!(
+            calc.stack.pop(),
+            None,
+            "Did not pop stack before pushing new value"
+        );
+        assert_eq!(
+            calc.operation_mode, -3,
+            "Didn't change operation mode to -3"
+        );
+    }
+
+    #[test]
+    fn test_dec_place_construction_third_place() {
+        // checks if the decimal_place_construction method
+        // correctly constructs decimal places from `2`
+        // and consumes literals
+
+        let mut calc = new_calc("45", -4);
+
+        // prepare stack -> skip integer construction part
+        calc.stack.push(Value::Float(1.23));
+        calc.decimal_place_construction('4');
+
+        assert_eq!(calc.cmd_stream.peek(), Some('5'), "Didn't consume command");
+        assert_eq!(
+            calc.stack.pop_float(),
+            Some(1.234),
+            "Didn't update top of stack"
+        );
+        assert_eq!(
+            calc.stack.pop(),
+            None,
+            "Did not pop stack before pushing new value"
+        );
+        assert_eq!(
+            calc.operation_mode, -5,
+            "Didn't change operation mode to -5"
+        );
+    }
+
+    #[test]
+    fn test_dec_place_construction_recv_dot() {
+        // checks if the decimal_place_construction method
+        // correctly constructs new float from `.`
+        // and consumes literal
+
+        let mut calc = new_calc(".2", -3);
+
+        // prepare stack -> skip integer construction part
+        calc.stack.push(Value::Float(1.0));
+        calc.decimal_place_construction('.');
+
+        assert_eq!(calc.cmd_stream.peek(), Some('2'), "Didn't consume command");
+        assert_eq!(
+            calc.stack.pop_float(),
+            Some(0.0),
+            "Didn't update top of stack"
+        );
+        assert_eq!(
+            calc.stack.pop_float(),
+            Some(1.0),
+            "Did pop stack before pushing new value"
+        );
+        assert_eq!(
+            calc.operation_mode, -2,
+            "Didn't change operation mode to -2"
+        );
+    }
+
+    #[test]
+    fn test_dec_place_construction_recv_other() {
+        // checks if the decimal_place_construction method
+        // correctly changes op mode from other like `a`
+        // and doesn't conusme literal
+
+        let mut calc = new_calc("a2", -3);
+
+        // prepare stack -> skip integer construction part
+        calc.stack.push(Value::Float(1.0));
+        calc.decimal_place_construction('a');
+
+        assert_eq!(
+            calc.cmd_stream.peek(),
+            Some('a'),
+            "Did consume `a`, but shouldn't"
+        );
+        assert_eq!(calc.stack.pop_float(), Some(1.0), "Updated top of stack");
+        assert_eq!(calc.stack.pop(), None, "Changed stack below top");
+        assert_eq!(calc.operation_mode, 0, "Didn't change operation mode to 0");
     }
 }
