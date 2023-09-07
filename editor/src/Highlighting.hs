50f2a8e45cc276745c6449a815cd3162958c8dac
Johannes Zottele <official@johannes-zottele.at>
2023-09-07 13:00:24 +0200

    editor: refactor highlighting

--- editor/src/Highlighting.hs
+++ editor/src/Highlighting.hs
@@ -1,38 +1,40 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE OverloadedLabels #-}
 
+-- | This module provides functions to initialize and apply syntax highlighting rules
+-- to a Gtk TextBuffer, using specified highlight conditions and rules.
 module Highlighting (
   HighlightRule,
   HighlightCond(Keys, Expr),
-  initializeHighlighting,
-  applyRules,
+  initializeHighlighting, -- ^ Initializes the highlighting by creating text tags for each rule and adding them to the text tag table.
+  applyRules,            -- ^ Applies the highlighting rules to the text present in the buffer, based on the provided separators.
 ) where
 
 import Data.GI.Base
 import qualified GI.Gtk as Gtk
 import qualified Data.Text as T
-import Control.Monad (when, forM)
+import Control.Monad (when, forM, forM_)
 
--- condition is either one of the keywords in Keys [String] or the expression Expr evaluates to true
-data HighlightCond = Keys [String]
-                    | Expr (String -> Bool)
+-- | Specifies the condition for highlighting a word: either being a member of a set of keywords (Keys),
+-- or satisfying a given predicate function (Expr).
+data HighlightCond = Keys [String] -- ^ A list of keyword strings to be highlighted.
+                   | Expr (String -> Bool) -- ^ A predicate function to determine if a string should be highlighted.
 
--- ( tagName, color, condition
+-- | Defines a syntax highlighting rule, with a name, foreground color and a condition under which to apply the rule.
 type HighlightRule = (String, String, HighlightCond)
 
-type Separators = [Char]
-type WordInfo = (T.Text, Int, Int)
+type Separators = [Char] -- ^ A list of characters to be considered as word separators.
+type WordInfo = (T.Text, Int, Int) -- ^ A tuple representing a word or separator in the text (string, start offset, end offset).
 
-
-initializeHighlighting:: [HighlightRule] -> Gtk.TextTagTable -> IO [Gtk.TextTag]
-initializeHighlighting rules tagTable = do
+-- | Initializes highlighting by creating and adding text tags for each rule in the tag table.
+initializeHighlighting :: [HighlightRule] -> Gtk.TextTagTable -> IO [Gtk.TextTag]
+initializeHighlighting rules tagTable =
   forM rules $ \(name, foregroundColor, _) -> do
     tag <- Gtk.new Gtk.TextTag [#name := T.pack name, #foreground := T.pack foregroundColor]
     _ <- #add tagTable tag
     return tag
 
-
--- applies highlights to text buffer based on found words that hold start and end iterator offsets
+-- | Applies the highlighting rules to the entire text in the buffer, removing any existing tags first.
 applyRules :: [HighlightRule] -> Separators -> Gtk.TextBuffer -> IO ()
 applyRules rules separators buffer = do
   start <- #getStartIter buffer
@@ -42,47 +44,47 @@ applyRules rules separators buffer = do
   text <- #getText buffer start end True
   let foundWords = getWords text separators
 
-  mapM_ (applyRule foundWords) rules
+  forM_ rules $ \(tagName, _, condition) -> do
+    tagTable <- #getTagTable buffer
+    maybeTag <- #lookup tagTable $ T.pack tagName
+    case maybeTag of
+      Just tag -> mapM_ (applyHighlight condition buffer tag) foundWords
+      Nothing  -> return ()
+
+-- | Applies a single highlight rule to a word in the text, based on the condition specified in the rule.
+applyHighlight :: HighlightCond -> Gtk.TextBuffer -> Gtk.TextTag -> WordInfo -> IO ()
+applyHighlight condition buffer tag (word, startOffset, endOffset) = do
+  case condition of
+    Keys keywords -> when (T.unpack word `elem` keywords) $ applyTag buffer tag startOffset endOffset
+    Expr expr     -> when (expr $ T.unpack word) $ applyTag buffer tag startOffset endOffset
+
+-- | Applies a text tag to a portion of text in the buffer, between the specified start and end offsets.
+applyTag :: Gtk.TextBuffer -> Gtk.TextTag -> Int -> Int -> IO ()
+applyTag buffer tag startOffset endOffset = do
+  startIter <- #getIterAtOffset buffer $ fromIntegral startOffset
+  endIter   <- #getIterAtOffset buffer $ fromIntegral (endOffset + 1)
+  #applyTag buffer tag startIter endIter
+
+-- | Splits the text into words and separators, with their respective start and end offsets.
+getWords :: T.Text -> Separators -> [WordInfo]
+getWords text separators = extractWords (0, T.unpack text)
   where
-    applyRule :: [WordInfo] -> HighlightRule -> IO ()
-    applyRule foundWords (tagName, _, condition) = do
-      tagTable <- #getTagTable buffer
-      maybeTag <- #lookup tagTable $ T.pack tagName
-      case maybeTag of
-        Just tag -> mapM_ (applyHighlight condition tag) foundWords
-        Nothing  -> return ()
-
-    applyHighlight:: HighlightCond -> Gtk.TextTag -> WordInfo -> IO ()
-    applyHighlight condition tag (word, startOffset, endOffset) = do
-      case condition of
-        Keys keywords -> when (T.unpack word `elem` keywords) (applyTag tag startOffset endOffset)
-        Expr expr     -> when (expr $ T.unpack word) (applyTag tag startOffset endOffset)
-
-    applyTag:: Gtk.TextTag -> Int -> Int -> IO ()
-    applyTag tag startOffset endOffset = do
-      startIter <- #getIterAtOffset buffer $ fromIntegral startOffset
-      endIter   <- #getIterAtOffset buffer $ fromIntegral (endOffset + 1)
-      #applyTag buffer tag startIter endIter
-
-
-
-getWords :: T.Text -> [Char] -> [WordInfo]
-getWords text separators = addLastWord $ foldl processChar (0, [], 0) (zip [0..] (T.unpack text))
-  where
-    processChar (wordLen, foundWords, startIndex) (i, c)
-      | c `elem` separators = if not (T.null currentWord)
-                                 then (0, foundWords ++ [(currentWord, startIndex, endIndex), (T.pack [c], i, i)], i + 1)
-                                 else (0, foundWords ++ [(T.pack [c], i, i)], i + 1)
-      | otherwise = (wordLen + 1, foundWords, if T.null currentWord then i else startIndex)
-      where
-        endIndex = i - 1
-        currentWord = T.take (fromIntegral wordLen) (T.drop (fromIntegral startIndex) text)
-
-    addLastWord :: (Int, [WordInfo], Int) -> [WordInfo]
-    addLastWord (wordLen, foundWords, startIndex) =
-      let endIndex = T.length text - 1
-          lastWord = T.take (endIndex - startIndex + 1) (T.drop startIndex text)
-      in if not (T.null lastWord) && wordLen > 0
-           then foundWords ++ [(lastWord, startIndex, endIndex)]
-           else foundWords
+    extractWords (_, []) = []
+    extractWords (i, xs) =
+      let 
+        -- This separates the input string 'xs' into a 'word' (sequence of characters not in 'separators') 
+        -- and the 'rest' of the string.
+        (word, rest) = span (`notElem` separators) xs 
+      
+        -- Calculate the length of the 'word'.
+        wordLen = length word 
+      
+        -- If 'rest' is not empty, create a list containing a tuple with information about the separator (which is 
+        -- the first character in 'rest'), otherwise create an empty list.
+        sepInfo = if null rest then [] else [(T.pack [head rest], i + wordLen, i + wordLen)] 
+      in 
+        -- Create a tuple containing information about the 'word', add it to the list followed by 'sepInfo', and 
+        -- recursively call 'extractWords' to process the rest of the string (excluding the first character which 
+        -- is a separator).
+        (T.pack word, i, i + wordLen - 1) : sepInfo ++ extractWords (i + wordLen + 1, if null rest then [] else tail rest)
 
