a6b45fcad23c3431af972ea824dbbec924e6a996
Paul Pinter <paul.pinter@fastmail.com>
2023-09-12 22:09:08 +0200

    add some more tests

--- interpreter/test/builtin/test_arithmetic.py
+++ interpreter/test/builtin/test_arithmetic.py
@@ -1,47 +1,117 @@
+import unittest
 from test.testhelper import exec_source
 from value import IntValue
 
 
-def test_plus():
-    source = """(+ 2 3)"""
+class TestBuiltinArithmetic(unittest.TestCase):
+    def test_plus(self):
+        source = """(+ 2 3)"""
+        result = exec_source(source).value
+        assert isinstance(result, IntValue)
+        assert result.value == 5
 
-    result = exec_source(source).value
+    def test_minus(self):
+        source = """(- 2 3)"""
+        result = exec_source(source).value
+        assert isinstance(result, IntValue)
+        assert result.value == -1
 
-    assert isinstance(result, IntValue)
-    assert result.value == 5
+    def test_divide(self):
+        source = """(/ 15 5)"""
+        result = exec_source(source).value
+        assert isinstance(result, IntValue)
+        assert result.value == 3
 
+    def test_times(self):
+        source = """(* 3 3)"""
+        result = exec_source(source).value
+        assert isinstance(result, IntValue)
+        assert result.value == 9
 
-def test_minus():
-    source = """(- 2 3)"""
+    def test_modulo(self):
+        source = """(% 5 5)"""
+        result = exec_source(source).value
+        assert isinstance(result, IntValue)
+        assert result.value == 0
 
-    result = exec_source(source).value
+    def test_plus_zero(self):
+        source = """(+ 5 0)"""
+        result = exec_source(source).value
+        assert isinstance(result, IntValue)
+        assert result.value == 5
 
-    assert isinstance(result, IntValue)
-    assert result.value == -1
+    def test_minus_zero(self):
+        source = """(- 5 0)"""
+        result = exec_source(source).value
+        assert isinstance(result, IntValue)
+        assert result.value == 5
 
+    def test_multiply_large_numbers(self):
+        source = """(* 1000 1000)"""
+        result = exec_source(source).value
+        assert isinstance(result, IntValue)
+        assert result.value == 1000000
 
-def test_divide():
-    source = """(/ 15 5)"""
+    def test_divide_one(self):
+        source = """(/ 1 1)"""
+        result = exec_source(source).value
+        assert isinstance(result, IntValue)
+        assert result.value == 1
 
-    result = exec_source(source).value
+    def test_modulo_one(self):
+        source = """(% 1 1)"""
+        result = exec_source(source).value
+        assert isinstance(result, IntValue)
+        assert result.value == 0
 
-    assert isinstance(result, IntValue)
-    assert result.value == 3
+    def test_multiply_by_one(self):
+        source = """(* 42 1)"""
+        result = exec_source(source).value
+        assert isinstance(result, IntValue)
+        assert result.value == 42
 
+    def test_multiply_by_zero(self):
+        source = """(* 42 0)"""
+        result = exec_source(source).value
+        assert isinstance(result, IntValue)
+        assert result.value == 0
 
-def test_times():
-    source = """(* 3 3)"""
+    def test_divide_by_large_number(self):
+        source = """(/ 1 1000)"""
+        result = exec_source(source).value
+        assert isinstance(result, IntValue)
+        assert result.value == 0
 
-    result = exec_source(source).value
+    def test_modulo_large_divisor(self):
+        source = """(% 5 100)"""
+        result = exec_source(source).value
+        assert isinstance(result, IntValue)
+        assert result.value == 5
 
-    assert isinstance(result, IntValue)
-    assert result.value == 9
+    def test_plus_large_numbers(self):
+        source = """(+ 999999 1)"""
+        result = exec_source(source).value
+        assert isinstance(result, IntValue)
+        assert result.value == 1000000
 
+    def test_minus_result_zero(self):
+        source = """(- 5 5)"""
+        result = exec_source(source).value
+        assert isinstance(result, IntValue)
+        assert result.value == 0
 
-def test_modulo():
-    source = """(% 5 5)"""
+    def test_divide_zero(self):
+        source = """(/ 0 5)"""
+        result = exec_source(source).value
+        assert isinstance(result, IntValue)
+        assert result.value == 0
 
-    result = exec_source(source).value
+    def test_modulo_zero(self):
+        source = """(% 0 5)"""
+        result = exec_source(source).value
+        assert isinstance(result, IntValue)
+        assert result.value == 0
 
-    assert isinstance(result, IntValue)
-    assert result.value == 0
+if __name__ == "__main__":
+    unittest.main()
+
--- interpreter/test/builtin/test_comparison.py
+++ interpreter/test/builtin/test_comparison.py
@@ -1,56 +1,106 @@
+import unittest
+
 from value import BoolValue
 from test.testhelper import exec_source
 
 
-def test_equlity_false():
-    source = """(= 2 3)"""
+class TestBuiltinComparison(unittest.TestCase):
+    def test_equality_false(self):
+        source = """(= 2 3)"""
+        result = exec_source(source).value
+        assert isinstance(result, BoolValue)
+        assert not result.value
 
-    result = exec_source(source).value
+    def test_equality_true(self):
+        source = """(= 2 2)"""
+        result = exec_source(source).value
+        assert isinstance(result, BoolValue)
+        assert result.value
 
-    assert isinstance(result, BoolValue)
-    assert not result.value
+    def test_greater_than(self):
+        source = """(> 2 1)"""
+        result = exec_source(source).value
+        assert isinstance(result, BoolValue)
+        assert result.value
+
+    def test_greater_equal(self):
+        source = """(>= 2 1)"""
+        result = exec_source(source).value
+        assert isinstance(result, BoolValue)
+        assert result.value
+
+    def test_less_than(self):
+        source = """(< 1 2)"""
+        result = exec_source(source).value
+        assert isinstance(result, BoolValue)
+        assert result.value
+
+    def test_less_equal(self):
+        source = """(<= 1 2)"""
+        result = exec_source(source).value
+        assert isinstance(result, BoolValue)
+        assert result.value
+
+    def test_equality_zero(self):
+        source = """(= 0 0)"""
+        result = exec_source(source).value
+        assert isinstance(result, BoolValue)
+        assert result.value
+
+    def test_not_equal(self):
+        source = """(= 2 3)"""
+        result = exec_source(source).value
+        assert isinstance(result, BoolValue)
+        assert not result.value
+
+    def test_greater_than_equal(self):
+        source = """(> 2 2)"""
+        result = exec_source(source).value
+        assert isinstance(result, BoolValue)
+        assert not result.value
+
+    def test_greater_than_zero(self):
+        source = """(> 1 0)"""
+        result = exec_source(source).value
+        assert isinstance(result, BoolValue)
+        assert result.value
+
+    def test_greater_equal_zero(self):
+        source = """(>= 0 0)"""
+        result = exec_source(source).value
+        assert isinstance(result, BoolValue)
+        assert result.value
+
+    def test_greater_equal_inverse(self):
+        source = """(>= 1 2)"""
+        result = exec_source(source).value
+        assert isinstance(result, BoolValue)
+        assert not result.value
+
+    def test_less_than_zero(self):
+        source = """(< 0 1)"""
+        result = exec_source(source).value
+        assert isinstance(result, BoolValue)
+        assert result.value
+
+    def test_less_than_equal(self):
+        source = """(<= 2 2)"""
+        result = exec_source(source).value
+        assert isinstance(result, BoolValue)
+        assert result.value
+
+    def test_less_equal_zero(self):
+        source = """(<= 0 0)"""
+        result = exec_source(source).value
+        assert isinstance(result, BoolValue)
+        assert result.value
+
+    def test_less_equal_inverse(self):
+        source = """(<= 2 1)"""
+        result = exec_source(source).value
+        assert isinstance(result, BoolValue)
+        assert not result.value
 
 
-def test_equlity_true():
-    source = """(= 2 2)"""
-
-    result = exec_source(source).value
-
-    assert isinstance(result, BoolValue)
-    assert result.value
-
-
-def test_greater_than():
-    source = """(> 2 1)"""
-
-    result = exec_source(source).value
-
-    assert isinstance(result, BoolValue)
-    assert result.value
-
-
-def test_greater_equal():
-    source = """(>= 2 1)"""
-
-    result = exec_source(source).value
-
-    assert isinstance(result, BoolValue)
-    assert result.value
-
-
-def test_less_than():
-    source = """(< 1 2)"""
-
-    result = exec_source(source).value
-
-    assert isinstance(result, BoolValue)
-    assert result.value
-
-
-def test_less_equal():
-    source = """(<= 1 2)"""
-
-    result = exec_source(source).value
-
-    assert isinstance(result, BoolValue)
-    assert result.value
+if __name__ == "__main__":
+    unittest.main()
